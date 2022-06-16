a57848c6367cf63c8dcc58737073765618ebada8
davidnene <davienesh4@gmail.com>
2022-06-16 22:00:35 +0300

    commit tree 30089405c4efe6f5c3416b94cb2b64dfee0d28fd

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const checkFirstLast = word.split(''); //split characters to an array
+  console.log(`First letter is ${checkFirstLast[0]} while last letter is ${checkFirstLast[checkFirstLast.length - 1]}`)
+  
+  return checkFirstLast.join('') === checkFirstLast.reverse().join('') //strict comparison
 }
 
+console.log(isPalindrome('hannah'))
 /* 
   Add your pseudocode here
+  functiom palindrome takes one arguement
+  checks whether first character strictly equals last character
+  returns true for strict equality and false if not equal
 */
 
 /*
   Add written explanation of your solution here
+  - First step is to find means of extracting the first character and last character of a word
+  - One way to do this is to use the .split inbuilt function which will split the characters
+  - and return an array
+  - We then extract character its index ie array[0]
+  - Then we extract the last character by checking the array length and subtracting one to
+  - capture its index since indices count from zer0 ie array[array.length - 1]
+  - We then check for strict comparison of the two
+  - array[0] === [array.length - 1] ?
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +39,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
